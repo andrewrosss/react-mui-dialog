e14920fa324510ee5b99e3ce1cb63b08116de5ad
Andrew Ross <andrew.ross.mail@gmail.com>
2021-03-12 03:54:34 -0500

    Satisfy the linter

--- test/smoke.test.tsx
+++ test/smoke.test.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-import { DialogProvider, useDialog } from '../src';
+import { DialogProvider, useDialog } from "../src";
 
 const Nested = () => {
   useDialog();
   return null;
 };
 
-describe('it', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
+describe("it", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(<DialogProvider />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
-  it('renders and calls the main hook without crashing', () => {
-    const div = document.createElement('div');
+  it("renders and calls the main hook without crashing", () => {
+    const div = document.createElement("div");
     ReactDOM.render(
       <DialogProvider>
         <Nested />
