7c160466276304a2e93f2c31e2d55a237a41871f
Andrew Ross <andrew.ross.mail@gmail.com>
2021-03-12 00:10:51 -0500

    Update test/

---
+++ test/smoke.test.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { DialogProvider, useDialog } from '../src';
+
+const Nested = () => {
+  useDialog();
+  return null;
+};
+
+describe('it', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<DialogProvider />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it('renders and calls the main hook without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(
+      <DialogProvider>
+        <Nested />
+      </DialogProvider>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+});
